3c54d8ee27fd704dd21af55581145c841e6a6789
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-01 06:28:44 +0700

    Added Message

---
+++ Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Stegosaurus
+{
+    class Message
+    {
+        public String fileName;
+        public FileStream stream;
+
+        // Returns true if success
+        public bool OpenFileRead(String fn)
+        {
+            try
+            {
+                stream = File.OpenRead(fn);
+            }
+            catch (IOException e)
+            {
+                Debug.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool OpenFileWrite(String fn)
+        {
+            try
+            {
+                stream = File.OpenWrite(fn);
+            }
+            catch (IOException e)
+            {
+                Debug.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        public byte[] ReadEncryptedBytes(Int32 offset, Int32 count)
+        {
+            byte[] originalBytes = new byte[count];
+            stream.Read(originalBytes, offset, count);
+
+            byte[] encryptedBytes = Vigenere.EncryptBuffer(originalBytes);
+
+            return encryptedBytes;
+        }
+    }
+}
