14962e862283b2b834c8ffcdf095336e5ac64da3
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-28 23:28:00 +0700

    Hide and Extracti header

--- Video.cs
+++ Video.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.IO;
 using AForge.Video;
 using AForge.Video.VFW;
 
@@ -18,6 +19,7 @@ namespace Stegosaurus
         private AVIReader reader;
         private AVIWriter writer;
         private int CurrentLength;
+        private static String HeaderFile = "header.txt";
 
         public Video(String input)
         {
@@ -26,12 +28,16 @@ namespace Stegosaurus
             reader.Open(input);
             ResetReadByte();
 
-            Bitmap bitmap = reader.GetNextFrame();
-            //extract header
+            ExtractHeader();
         }
 
-        public Video(String input, String output, int length, int extension, int bytePerFrame, int LSB)
+        public Video(String input, String output, int length, String extension, int bytePerFrame, int LSB)
         {
+            Length = length;
+            Extension = extension;
+            BytePerFrame = bytePerFrame;
+            this.LSB = LSB;
+
             reader = new AVIReader();
             writer = new AVIWriter();
 
@@ -39,9 +45,7 @@ namespace Stegosaurus
             writer.Open(output, reader.Width, reader.Height);
             ResetWriteByte();
 
-            Bitmap bitmap = reader.GetNextFrame();
-            //hide header
-            writer.AddFrame(bitmap);
+            HideHeader();
         }
 
         public void InsertToFrame(byte[] insertedByte)
@@ -148,6 +152,144 @@ namespace Stegosaurus
             }
             CurrentLength += BytePerFrame;
             return ret;
+        }
+
+        private void ExtractHeader()
+        {
+            Bitmap bitmap = reader.GetNextFrame();
+            int pos = 0;
+            int len = 0;
+
+            // getting length header
+            for (int i = 0; i < 4; i++)
+            {
+                int nowByte = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    int x = pos / bitmap.Size.Width;
+                    int y = pos % bitmap.Size.Width;
+
+                    byte r = bitmap.GetPixel(x, y).R;
+                    byte g = bitmap.GetPixel(x, y).G;
+                    byte b = bitmap.GetPixel(x, y).B;
+
+                    if (LSB == 1)
+                    {
+                        nowByte += ((1 << (j * 3)) & r);
+                        nowByte += ((1 << (j * 3 + 1)) & g);
+                        if (j < 2)
+                        {
+                            nowByte += ((1 << (j * 3 + 2)) & b);
+                        }
+                    }
+                    else if (LSB == 2)
+                    {
+                        nowByte += ((1 << (j * 3)) & r);
+                        nowByte += ((1 << (j * 3 + 1)) & r);
+                        if (j < 2)
+                        {
+                            nowByte += ((1 << (j * 3 + 2)) & g);
+                            nowByte += ((1 << (j * 3 + 3)) & g);
+                        }
+                        if (j < 2)
+                        {
+                            nowByte += ((1 << (j * 3 + 4)) & b);
+                            nowByte += ((1 << (j * 3 + 5)) & b);
+                        }
+                    }
+                    pos++;
+                }
+                len += nowByte;
+            }
+
+            //getting header
+            byte[] ret = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                byte nowByte = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    int x = pos / bitmap.Size.Width;
+                    int y = pos % bitmap.Size.Width;
+
+                    byte r = bitmap.GetPixel(x, y).R;
+                    byte g = bitmap.GetPixel(x, y).G;
+                    byte b = bitmap.GetPixel(x, y).B;
+
+                    if (LSB == 1)
+                    {
+                        nowByte += (byte)((1 << (j * 3)) & r);
+                        nowByte += (byte)((1 << (j * 3 + 1)) & g);
+                        if (j < 2)
+                        {
+                            nowByte += (byte)((1 << (j * 3 + 2)) & b);
+                        }
+                    }
+                    else if (LSB == 2)
+                    {
+                        nowByte += (byte)((1 << (j * 3)) & r);
+                        nowByte += (byte)((1 << (j * 3 + 1)) & r);
+                        if (j < 2)
+                        {
+                            nowByte += (byte)((1 << (j * 3 + 2)) & g);
+                            nowByte += (byte)((1 << (j * 3 + 3)) & g);
+                        }
+                        if (j < 2)
+                        {
+                            nowByte += (byte)((1 << (j * 3 + 4)) & b);
+                            nowByte += (byte)((1 << (j * 3 + 5)) & b);
+                        }
+                    }
+
+                    pos++;
+                }
+                ret[i] = nowByte;
+            }
+
+            // output to header file
+            FileStream fs = File.OpenWrite(HeaderFile);
+            fs.Write(ret, 0, len);
+            fs.Close();
+
+            // read from Header
+            TextReader tr = new StreamReader(HeaderFile);
+            Length = Convert.ToInt32(tr.ReadLine());
+            Extension = tr.ReadLine();
+            BytePerFrame = Convert.ToInt32(tr.ReadLine());
+            LSB = Convert.ToInt32(tr.ReadLine());
+            tr.Close();
+        }
+
+        private void HideHeader()
+        {
+            // create header file
+            TextWriter tw = new StreamWriter(HeaderFile);
+            tw.WriteLine(Length);
+            tw.WriteLine(Extension);
+            tw.WriteLine(BytePerFrame);
+            tw.WriteLine(LSB);
+            tw.Close();
+
+            // get stream byte from header file
+            FileStream fs = File.OpenRead(HeaderFile);
+            byte[] temp = new byte[fs.Length];
+            fs.Read(temp, 0, Convert.ToInt32(fs.Length));
+
+            // add 4 byte before the real header
+            byte[] bytes = new byte[fs.Length + 4];
+            int pos = 0;
+            for (int i = 0, k = 0; i < 4; i++)
+            {
+                bytes[pos++] = (byte) ((Convert.ToInt32(fs.Length) / (1 << k)) % (1 << 8));
+                k += 8;
+            }
+            for (int i = 0; i < Convert.ToInt32(fs.Length); i++)
+            {
+                bytes[pos++] = temp[i];
+            }
+
+            fs.Close();
+            InsertToFrame(bytes);
         }
 
         public void ResetReadByte()
