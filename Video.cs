5a380feeefc72256b09be552d6e207e5f624ba1e
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-28 19:48:48 +0700

    added Video reader dan writer

---
+++ Video.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using AForge.Video;
+using AForge.Video.VFW;
+
+namespace Stegosaurus
+{
+    class Video
+    {
+        public int Length { get; private set; }
+        public String Extension { get; private set; }
+        public int BytePerFrame { get; private set; }
+        public int LSB { get; private set; }
+
+        private AVIReader reader;
+        private AVIWriter writer;
+
+        public Video(String input, String output, int length, int extension, int bytePerFrame, int LSB)
+        {
+            reader = new AVIReader();
+            writer = new AVIWriter();
+
+            reader.Open(input);
+
+            Bitmap bitmap = reader.GetNextFrame();
+        }
+
+        public void InsertToFrame(byte[] insertedByte)
+        {
+            Bitmap bitmap = reader.GetNextFrame();
+        }
+
+        public byte[] GetByteFromFrame()
+        {
+            return null;
+        }
+    }
+}
