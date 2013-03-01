8e0f27b3662b7e340e6ab1c995d18ed9234b857f
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-01 07:31:52 +0700

    Update Engine

--- Engine.cs
+++ Engine.cs
@@ -6,20 +6,20 @@ using System.Text;
 namespace Stegosaurus
 {
     // Main application logic class. A singleton.
-    class Engine
+    public class Engine
     {
         public static string SourceMessageFileName { get; set; }
         public static string SourceVideoFileName { get; set; }
         public static string OutputMessageFileName { get; set; }
         public static string OutputVideoFileName { get; set; }
         public static string Key { get; set; }
-        public static string LsbMode { get; set; }
+        public static int LsbMode { get; set; }
 
         // TODO
         public static int BytePerFrame = 4096;
 
         // Precondition: all relevant properties have proper values.
-        public void EncryptAndSave()
+        public static void EncryptAndSave()
         {
             var videoHandler = new Video(SourceVideoFileName);
         }
