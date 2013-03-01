b3d53e5d7acc0dc41b1143341dac8019c3d56fc6
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-01 07:24:06 +0700

    Add Stegosaurus.cs
    Main business logic goes here

---
+++ Stegosaurus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stegosaurus
+{
+    // Main application logic class. A singleton.
+    class Stegosaurus
+    {
+        public static string SourceMessageFileName { get; set; }
+        public static string SourceVideoFileName { get; set; }
+        public static string OutputMessageFileName { get; set; }
+        public static string OutputVideoFileName { get; set; }
+        public static string Key { get; set; }
+        public static string LsbMode { get; set; }
+
+        // TODO
+        public static int BytePerFrame = 4096;
+
+        // Precondition: all relevant properties have proper values.
+        public void EncryptAndSave()
+        {
+            var videoHandler = new Video(SourceVideoFileName);
+        }
+    }
+}
